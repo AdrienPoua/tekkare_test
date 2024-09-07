a6c5f60ba0bee81841386f9d51e02a18e1695eb0
zartax95 <adrienvilb@msn.com>
2024-09-07 10:47:29 +0200

    Initial commit, libs added, tailwind & ts  config setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from '@/components/Header'
+import DashboardLayout from '@/layout'
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <DashboardLayout>
+      <Header />
+      <Button>
+        Hellooooo
+      </Button>
+    </DashboardLayout>
+  );
+}
+
+export default App;
